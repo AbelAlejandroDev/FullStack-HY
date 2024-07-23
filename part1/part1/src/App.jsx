940b343ca9de2fbbb4a91a090e0bed167fd4e7e7
AbelAlejandroDev <pyschofaint@gmail.com>
2024-07-23 09:52:44 -0400

    Ejerc 1.0 done

---
+++ part1/part1/src/App.jsx
@@ -0,0 +1,33 @@
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya',]
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+export default App
